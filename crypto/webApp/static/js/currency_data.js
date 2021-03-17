d9fb75227e4f72da593d263c3e1b012566b52e41
Ronan Byrne <ronan.byrne1@ucdconnect.ie>
2021-03-17 22:45:32 +0000

    Step 8: Added basic styling for the market table

--- crypto/webApp/static/js/currency_data.js
+++ crypto/webApp/static/js/currency_data.js
@@ -3,28 +3,36 @@ const Interval_Time = 60000 * 2
 
 let market_ajax_call = function(){
     $.get("api/realtime", function(data, status) {
-        console.log(data)
         let table = document.getElementById('table_body');
         let updatedHTML = '';
         data.forEach(coin => {
-            row =   "<tr>"+
-                        "<td> <img class='coin_image' src=" + coin.image + "/></td>" +
-                        "<td>" + coin.name + "</td>" +
-                        "<td>" + coin.current_price + "</td>" +
-                        "<td>" + coin.market_cap + "</td>" +
-                        "<td>" + coin.price_change_24h + "</td>" +
-                    "</tr>"
+            if(coin.price_change_24h < 0){
+                row =   "<tr>"+
+                            "<td> <img class='coin_image' src=" + coin.image + "/></td>" +
+                            "<td>" + coin.name + "</td>" +
+                            "<td>" + coin.current_price + "</td>" +
+                            "<td>" + coin.market_cap + "</td>" +
+                            "<td class='red'>" + coin.price_change_24h + "</td>" +
+                        "</tr>"
+
+            } else {
+                row =   "<tr>"+
+                            "<td> <img class='coin_image' src=" + coin.image + "/></td>" +
+                            "<td>" + coin.name + "</td>" +
+                            "<td>" + coin.current_price + "</td>" +
+                            "<td>" + coin.market_cap + "</td>" +
+                            "<td class='green'>" + coin.price_change_24h + "</td>" +
+                        "</tr>"
+            }
             updatedHTML += row;
         })   
         table.innerHTML = updatedHTML;     
     })
 };
 
-$(document).ready( function () {
-    $('#table_id').DataTable();
-} );
-
 $(document).ready(market_ajax_call());
 
 //Perform update every 2 minutes
-//setInterval(ajax_call, interval);
+setInterval(market_ajax_call, Interval_Time);
+
+document.getElementById('refresh_btn').addEventListener('click', market_ajax_call);
